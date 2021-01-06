8d1228fc57c7b46ed9f486f96e5e5a4ab4cec052
gbonds <ge.bonds@gmail.com>
2021-01-06 15:03:03 -0500

    Added function to fire and mark cells as hit/miss

--- gameplay.js
+++ gameplay.js
@@ -27,14 +27,15 @@ var view = {
 };
 
 //// testing view object
-view.displayMiss('11');
-view.displayMiss('13');
-view.displayHit('15');
-view.displayHit('16');
-view.displayHit('17');
-view.displayMessage('Testing one, two, three.');
+//view.displayMiss('01');
+//view.displayMiss('53');
+//view.displayHit('15');
+//view.displayHit('16');
+//view.displayHit('17');
+//view.displayMessage('Testing one, two, three.');
 
 
+// sets board settings and fire method
 var model = {
     // sets board size, ship length, and number of ships for future customization
     boardSize: 7,
@@ -46,9 +47,49 @@ var model = {
 
     // sets ship locations
     //// TODO make ship locations random
-    ships: [{ locations: ['17', '27', '37'], hits: ['', '', ''] },
-    { locations: ['35', '45', '55'], hits: ['', '', ''] },
-    { locations: ['21', '22', '23'], hits: ['', '', ''] }
-    ]
+    ships: [{ locations: ['06', '16', '26'], hits: ['', '', ''] },
+    { locations: ['24', '34', '44'], hits: ['', '', ''] },
+    { locations: ['10', '11', '12'], hits: ['', '', ''] }
+    ],
 
+    // sets fire based on user guess
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ships.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                // if all ship locations hits, will add to shipsSunk count
+                if (this.shipsSunk(ship)) {
+                    view.displayMessage("You sank my battleship! There are " + (numShips - shipsSunk) + " ships still afloat.")
+                    this.shipsSunk++
+                }
+                return true;
+            }
+        } // for loop ends
+
+        // if user guess is a miss
+        view.displayMiss(guess);
+        view.displayMessage("MISS!");
+        return false;
+
+    }, // fire ends
+
+    isSunk: function (ship) {
+
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        } // for loop ends
+
+    } // isSunk ends
 };
+
+//// testing model
+model.fire("53");
+//// TODO figure out why fires below aren't registering on board
+model.fire("06");
+model.fire("16");
